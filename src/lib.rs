3bc0db2c17c0a5663f179a36deea45cc2d3828db
Taiki Endo <te316e89@gmail.com>
2023-01-08 00:48:30 +0900

    Change crate-level doc style

--- src/lib.rs
+++ src/lib.rs
@@ -1,108 +1,110 @@
-//! A procedural macro helper for easily writing [derives macros][proc-macro-derive] for enums.
-//!
-//! # Examples
-//!
-//! [`quick_derive!`] macro make easy to write [`proc_macro_derive`][proc-macro-derive]
-//! like deriving trait to enum so long as all variants are implemented that trait.
-//!
-//! ```rust
-//! # extern crate proc_macro;
-//! #
-//! use derive_utils::quick_derive;
-//! use proc_macro::TokenStream;
-//!
-//! # #[cfg(any())]
-//! #[proc_macro_derive(Iterator)]
-//! # pub fn _derive_iterator(_: TokenStream) -> TokenStream { unimplemented!() }
-//! pub fn derive_iterator(input: TokenStream) -> TokenStream {
-//!     quick_derive! {
-//!         input,
-//!         // trait path
-//!         std::iter::Iterator,
-//!         // trait definition
-//!         trait Iterator {
-//!             type Item;
-//!             fn next(&mut self) -> Option<Self::Item>;
-//!             fn size_hint(&self) -> (usize, Option<usize>);
-//!         }
-//!     }
-//! }
-//!
-//! # #[cfg(any())]
-//! #[proc_macro_derive(ExactSizeIterator)]
-//! # pub fn _derive_exact_size_iterator(_: TokenStream) -> TokenStream { unimplemented!() }
-//! pub fn derive_exact_size_iterator(input: TokenStream) -> TokenStream {
-//!     quick_derive! {
-//!         input,
-//!         // trait path
-//!         std::iter::ExactSizeIterator,
-//!         // super trait's associated types
-//!         <Item>,
-//!         // trait definition
-//!         trait ExactSizeIterator: Iterator {
-//!             fn len(&self) -> usize;
-//!         }
-//!     }
-//! }
-//! ```
-//!
-//! ### Generated code
-//!
-//! When deriving for enum like the following:
-//!
-//! ```rust
-//! # #[cfg(any())]
-//! #[derive(Iterator, ExactSizeIterator, Future)]
-//! # struct _Enum<A>(A);
-//! enum Enum<A, B> {
-//!     A(A),
-//!     B(B),
-//! }
-//! ```
-//!
-//! Code like this will be generated:
-//!
-//! ```rust
-//! enum Enum<A, B> {
-//!     A(A),
-//!     B(B),
-//! }
-//!
-//! impl<A, B> std::iter::Iterator for Enum<A, B>
-//! where
-//!     A: std::iter::Iterator,
-//!     B: std::iter::Iterator<Item = <A as std::iter::Iterator>::Item>,
-//! {
-//!     type Item = <A as std::iter::Iterator>::Item;
-//!     fn next(&mut self) -> Option<Self::Item> {
-//!         match self {
-//!             Enum::A(x) => x.next(),
-//!             Enum::B(x) => x.next(),
-//!         }
-//!     }
-//!     fn size_hint(&self) -> (usize, Option<usize>) {
-//!         match self {
-//!             Enum::A(x) => x.size_hint(),
-//!             Enum::B(x) => x.size_hint(),
-//!         }
-//!     }
-//! }
-//!
-//! impl<A, B> std::iter::ExactSizeIterator for Enum<A, B>
-//! where
-//!     A: std::iter::ExactSizeIterator,
-//!     B: std::iter::ExactSizeIterator<Item = <A as Iterator>::Item>,
-//! {
-//!     fn len(&self) -> usize {
-//!         match self {
-//!             Enum::A(x) => x.len(),
-//!             Enum::B(x) => x.len(),
-//!         }
-//!     }
-//! }
-//! ```
-//!
-//! [proc-macro-derive]: https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros
+/*!
+A procedural macro helper for easily writing [derives macros][proc-macro-derive] for enums.
+
+# Examples
+
+[`quick_derive!`] macro make easy to write [`proc_macro_derive`][proc-macro-derive]
+like deriving trait to enum so long as all variants are implemented that trait.
+
+```rust
+# extern crate proc_macro;
+#
+use derive_utils::quick_derive;
+use proc_macro::TokenStream;
+
+# #[cfg(any())]
+#[proc_macro_derive(Iterator)]
+# pub fn _derive_iterator(_: TokenStream) -> TokenStream { unimplemented!() }
+pub fn derive_iterator(input: TokenStream) -> TokenStream {
+    quick_derive! {
+        input,
+        // trait path
+        std::iter::Iterator,
+        // trait definition
+        trait Iterator {
+            type Item;
+            fn next(&mut self) -> Option<Self::Item>;
+            fn size_hint(&self) -> (usize, Option<usize>);
+        }
+    }
+}
+
+# #[cfg(any())]
+#[proc_macro_derive(ExactSizeIterator)]
+# pub fn _derive_exact_size_iterator(_: TokenStream) -> TokenStream { unimplemented!() }
+pub fn derive_exact_size_iterator(input: TokenStream) -> TokenStream {
+    quick_derive! {
+        input,
+        // trait path
+        std::iter::ExactSizeIterator,
+        // super trait's associated types
+        <Item>,
+        // trait definition
+        trait ExactSizeIterator: Iterator {
+            fn len(&self) -> usize;
+        }
+    }
+}
+```
+
+### Generated code
+
+When deriving for enum like the following:
+
+```rust
+# #[cfg(any())]
+#[derive(Iterator, ExactSizeIterator, Future)]
+# struct _Enum<A>(A);
+enum Enum<A, B> {
+    A(A),
+    B(B),
+}
+```
+
+Code like this will be generated:
+
+```rust
+enum Enum<A, B> {
+    A(A),
+    B(B),
+}
+
+impl<A, B> std::iter::Iterator for Enum<A, B>
+where
+    A: std::iter::Iterator,
+    B: std::iter::Iterator<Item = <A as std::iter::Iterator>::Item>,
+{
+    type Item = <A as std::iter::Iterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Enum::A(x) => x.next(),
+            Enum::B(x) => x.next(),
+        }
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            Enum::A(x) => x.size_hint(),
+            Enum::B(x) => x.size_hint(),
+        }
+    }
+}
+
+impl<A, B> std::iter::ExactSizeIterator for Enum<A, B>
+where
+    A: std::iter::ExactSizeIterator,
+    B: std::iter::ExactSizeIterator<Item = <A as Iterator>::Item>,
+{
+    fn len(&self) -> usize {
+        match self {
+            Enum::A(x) => x.len(),
+            Enum::B(x) => x.len(),
+        }
+    }
+}
+```
+
+[proc-macro-derive]: https://doc.rust-lang.org/reference/procedural-macros.html#derive-macros
+*/
 
 #![doc(test(
     no_crate_inject,
