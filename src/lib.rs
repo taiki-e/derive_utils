45b161378a0733251b5d01c9d078639bf257137e
Taiki Endo <te316e89@gmail.com>
2026-02-27 22:19:17 +0900

    Update allowed lint list

--- src/lib.rs
+++ src/lib.rs
@@ -139,9 +139,9 @@ where
     clippy::exhaustive_enums,
     clippy::exhaustive_structs,
     clippy::impl_trait_in_params,
-    // clippy::missing_inline_in_public_items,
     clippy::std_instead_of_alloc,
     clippy::std_instead_of_core,
+    // clippy::missing_inline_in_public_items,
 )]
 #![allow(clippy::must_use_candidate)]
 
